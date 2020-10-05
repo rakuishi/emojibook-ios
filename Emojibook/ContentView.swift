39d32bf5dd2cff91ffcb3ea534bb7c8c9c2a5986
rakuishi <rakuishi@gmail.com>
2020-10-05 21:39:00 +0900

    First commit

---
+++ Emojibook/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Emojibook
+//
+//  Created by Koichiro OCHIISHI on 2020/10/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Emojibook/EmojibookApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojibookApp.swift
+//  Emojibook
+//
+//  Created by Koichiro OCHIISHI on 2020/10/05.
+//
+
+import SwiftUI
+
+@main
+struct EmojibookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
